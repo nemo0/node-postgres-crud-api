0c9910863c47b2318f7e596688ea6d443cff6d07
Subha Chanda <subhachanda88@gmail.com>
2022-05-29 23:39:12 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+const db = require('./queries');
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' });
+});
+
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
+app.post('/users', db.createUser);
+app.put('/users/:id', db.updateUser);
+app.delete('/users/:id', db.deleteUser);
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
